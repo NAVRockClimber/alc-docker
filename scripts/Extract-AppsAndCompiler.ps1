9101da2ccc1b5d694037af795cbaf232c4e93323
Bert Krätge <bert@kraetge.de>
2020-04-08 21:22:48 +0200

    move Apps from folder to our destination

--- scripts/Extract-AppsAndCompiler.ps1
+++ scripts/Extract-AppsAndCompiler.ps1
@@ -1,13 +1,9 @@
-$ServiceManagementModule = (Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Service\Microsoft.Dynamics.Nav.Apps.Management.psd1").FullName
-Import-Module $ServiceManagementModule -DisableNameChecking
-
 New-Item -Path "C:\alc\depedencies" -ItemType Directory -Force
 New-Item -Path "C:\alc\compiler" -ItemType Directory -Force
 
-$apps = Get-NAVAppInfo -ServerInstance BC
+$apps = Get-ChildItem -Path C:\Applications\ -Filter *.app -Recurse
 foreach ($app in $apps) {
-    $AppFileName = Join-Path -Path "C:\alc\depedencies" -ChildPath "$($app.Name).app"
-    Write-Host "Exporting $($app.Name) to $($AppFileName)"
-    Get-NavAppRuntimePackage -ServerInstance BC -Name $app.Name -Path $AppFileName
-    Write-Host "Finished"
+    $AppFileName = Join-Path -Path "C:\alc\depedencies" -ChildPath $app.Name
+    Write-Host "Copying $($app.Name) to $($AppFileName)"
+    Copy-Item -Path $app.FullName -Destination $AppFileName -Force
 }
