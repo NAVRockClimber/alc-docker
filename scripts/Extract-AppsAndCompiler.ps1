775e10bf73fc7cf802349c27f624fb12247ad35d
Bert Krätge <bert@kraetge.de>
2020-04-08 21:23:02 +0200

    copy compiler

--- scripts/Extract-AppsAndCompiler.ps1
+++ scripts/Extract-AppsAndCompiler.ps1
@@ -1,5 +1,6 @@
 New-Item -Path "C:\alc\depedencies" -ItemType Directory -Force
 New-Item -Path "C:\alc\compiler" -ItemType Directory -Force
+New-Item -Path "C:\Build" -ItemType Directory -Force
 
 $apps = Get-ChildItem -Path C:\Applications\ -Filter *.app -Recurse
 foreach ($app in $apps) {
@@ -7,3 +8,10 @@ foreach ($app in $apps) {
     Write-Host "Copying $($app.Name) to $($AppFileName)"
     Copy-Item -Path $app.FullName -Destination $AppFileName -Force
 }
+
+$tempZip = Join-Path -Path $env:TEMP -ChildPath "alc.zip"
+Copy-item -Path (Get-Item -Path "c:\run\*.vsix").FullName -Destination $tempZip
+Expand-Archive -Path $tempZip -DestinationPath "c:\build\vsix"
+Copy-Item -Path C:\Build\vsix\extension\bin\* -Destination C:\alc\compiler -Recurse
+
+Write-Host "Finished"
