07b92dd9daf0744d5c3e7fa90e164c7300e5b1ab
Bert Krätge <bert@kraetge.de>
2020-04-18 21:28:35 +0200

    call al compiler

--- scripts/Compile-AlApp.ps1
+++ scripts/Compile-AlApp.ps1
@@ -5,3 +5,6 @@ Write-Host $env:assemblyprobingpaths
 Write-Host $env:project
 Write-Host $env:out
 Write-Host $env:packagecachepath
+
+Write-Host "Running alc"
+\alc\compiler\alc.exe /project:$env:project /out:$env:out /packagecachepath:$env:packagecachepath /assemblyprobingpaths:$env:assemblyprobingpaths
