7a8aa7ac8818fa4e48bfda1cb9f0a3e66b9c5bdd
Bert Krätge <bert@kraetge.de>
2020-05-24 16:33:36 +0200

    compile app and export log

--- scripts/Compile-AlApp.ps1
+++ scripts/Compile-AlApp.ps1
@@ -13,24 +13,42 @@ foreach ($folder in $assemblyFolders) {
     Write-Host ("Assembly Path: {0}" -f $folder.FullName)
 }
 $ProjectAssemblies = Join-Path -Path $env:project -ChildPath ".netpackages"
-Join-Path -Path $env:project -ChildPath ".netpackages"
 if (Test-Path $ProjectAssemblies) {
     $assemblyProbingPaths += '"' + ($ProjectAssemblies) + '"'
+    Write-Host ("Assembly Path: {0}" -f $ProjectAssemblies)
 }
 if (Test-Path -Path $env:assemblyprobingpaths) {
     $assemblyProbingPaths += '"' + ($env:assemblyprobingpaths) + '"'
+    Write-Host ("Assembly Path: {0}" -f $env:assemblyprobingpaths)
 }
 $assemblyProbingPaths += '"' + "c:\windows\assembly" + '"'
 $alcassemblyProbingPath = $assemblyProbingPaths -join ","
-Write-Host ("Total Assembly Path {0}" -f $alcassemblyProbingPath)
+Write-Host ("Total assembly probing path {0}" -f $alcassemblyProbingPath)
 
+$AppJsonFile = Get-ChildItem -Path $env:project -Filter app.json
+$AppJson = Get-Content -Raw -Path $AppJsonFile.FullName | ConvertFrom-Json
+$PublisherName = $AppJson.publisher
+$AppName = $AppJson.name 
+# $ArtifactName = '"' + (Join-Path -Path "C:\tmp\artifact" -ChildPath ("{0}_{1}.app" -f $PublisherName, $AppName)) + '"'
+$ArtifactName = '"' + (Join-Path -Path $env:out -ChildPath ("{0}_{1}.app" -f $PublisherName, $AppName)) + '"'
+
+# There seem to be circumstances where it faster to copy than write directly to share volume
+# $TempErrorLogFile = Join-Path -Path "C:\tmp\logs\" -ChildPath "error.log"
 
 Write-Host "Running alc"
-if (Test-Path -Path "\alc\compiler\win32")
+try
 {
-    Write-Host "Found dotnet core compiler." 
-    Write-Host "Using dotnet core compiler."
-    \alc\compiler\win32\alc.exe /project:$env:project /out:$env:out /packagecachepath:$env:packagecachepath /assemblyprobingpaths:$alcassemblyProbingPath /errorlog:$env:errorlog
-} else {
-    \alc\compiler\alc.exe /project:$env:project /out:$env:out /packagecachepath:$env:packagecachepath /assemblyprobingpaths:$alcassemblyProbingPath /errorlog:$env:errorlog
+    if (Test-Path -Path "\alc\compiler\win32")
+    {
+        Write-Host "Found dotnet core compiler." 
+        Write-Host "Using dotnet core compiler."
+        \alc\compiler\win32\alc.exe /project:$env:project /out:$ArtifactName /packagecachepath:$env:packagecachepath /assemblyprobingpaths:$alcassemblyProbingPath /errorlog:$env:errorlog
+    } else {
+        \alc\compiler\alc.exe /project:$env:project /out:$ArtifactName /packagecachepath:$env:packagecachepath /assemblyprobingpaths:$alcassemblyProbingPath /errorlog:$env:errorlog
+    }
+} catch {
+    Write-Host "An error occurred. Trying to write error log."
+    Move-Item -Path $TempErrorLogFile -Destination $TargetErrorLogFile -Filter *.* -Force
 }
+
+# Move-Item -Path $TempErrorLogFile -Destination $env:errorlog -Force
--- scripts/Extract-AppsAndCompiler.ps1
+++ scripts/Extract-AppsAndCompiler.ps1
@@ -5,7 +5,7 @@ New-Item -Path "C:\alc\assemblies" -ItemType Directory -Force | Out-Null
 New-Item -Path "C:\Build" -ItemType Directory -Force | Out-Null
 
 # Copy app files into create folder structure
-Write-Host "-------------------------------------------------------------------------"
+Write-Host "----------------------------------------------------------------------------------------------------"
 Write-Host "Copying app files"
 $apps = Get-ChildItem -Path C:\Applications\ -Filter *.app -Recurse
 foreach ($app in $apps) {
@@ -15,7 +15,7 @@ foreach ($app in $apps) {
 }
 
 # Get AL Compiler and move it into created folder structure
-Write-Host "-------------------------------------------------------------------------"
+Write-Host "----------------------------------------------------------------------------------------------------"
 Write-Host "Extracting Compiler"
 $tempZip = Join-Path -Path $env:TEMP -ChildPath "alc.zip"
 Copy-item -Path (Get-Item -Path "c:\run\*.vsix").FullName -Destination $tempZip
@@ -23,7 +23,7 @@ Expand-Archive -Path $tempZip -DestinationPath "c:\build\vsix"
 Copy-Item -Path C:\Build\vsix\extension\bin\* -Destination C:\alc\compiler -Recurse
 
 # Copy assemblies into created structure
-Write-Host "-------------------------------------------------------------------------"
+Write-Host "----------------------------------------------------------------------------------------------------"
 Write-Host "Copy assemblies"
 $roleTailoredClientFolder = "C:\Program Files (x86)\Microsoft Dynamics NAV\*\RoleTailored Client"
 $xmlLibFolder = "C:\Program Files (x86)\Open XML SDK\V2.5\lib"
@@ -48,7 +48,7 @@ $url = "https://makemeapassword.ligos.net/api/v1/alphanumeric/json?c=1&l=12"
 $Password = (Invoke-RestMethod -ContentType "application/json" -Uri $url).pws
 
 # Set environment variables and fire up BC
-Write-Host "-------------------------------------------------------------------------"
+Write-Host "----------------------------------------------------------------------------------------------------"
 Write-Host "Extract System App"
 # Better pass this though to give the user the possibility to accept Microsofts EULA
 $env:Accept_eula = $env:Accept_MS_Eula
@@ -60,6 +60,7 @@ Write-Host ("Generated Password: {0}" -f $env:password)
 & c:/Run/navstart.ps1 
 
 # Extract System app file from service
+Write-Host "----------------------------------------------------------------------------------------------------"
 Write-Host ("Extracting System App")
 $publisher = "Microsoft"
 $appName="System"
@@ -74,6 +75,6 @@ $headers = @{ Authorization = $basicAuthValue }
 $authParam += @{ "headers" = $headers }
 $appFile = ("C:\alc\depedencies\{0}_{1}.app" -f $publisher, $appName)
 Invoke-RestMethod -ContentType "application/octet-stream" -Method Get -Uri $url @AuthParam -OutFile $appFile 
-Write-Host "-------------------------------------------------------------------------"
+Write-Host "----------------------------------------------------------------------------------------------------"
 Write-Host "Finished extracting compiler and dependencies"
 Write-Host "Docker will take a while for committing everything and going on."
