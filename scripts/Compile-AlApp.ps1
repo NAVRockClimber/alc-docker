6fc1cfd90c595e407c103f42afcddd127a4d3c9a
Bert Krätge <bert@kraetge.de>
2020-05-25 15:28:47 +0200

    Add functionallity for subfolders

--- scripts/Compile-AlApp.ps1
+++ scripts/Compile-AlApp.ps1
@@ -21,19 +21,36 @@ if (Test-Path -Path $env:assemblyprobingpaths) {
     $assemblyProbingPaths += '"' + ($env:assemblyprobingpaths) + '"'
     Write-Host ("Assembly Path: {0}" -f $env:assemblyprobingpaths)
 }
-$assemblyProbingPaths += '"' + "c:\windows\assembly" + '"'
+$WindowAssembliesPath = "c:\windows\assembly"
+if (Test-Path -Path $WindowAssembliesPath)
+{
+    $assemblyProbingPaths += '"' + $WindowAssembliesPath + '"'
+    Write-Host ("Windows Assembly Path: {0}" -f $WindowAssembliesPath)
+}
 $alcassemblyProbingPath = $assemblyProbingPaths -join ","
 Write-Host ("Total assembly probing path {0}" -f $alcassemblyProbingPath)
 
-$AppJsonFile = Get-ChildItem -Path $env:project -Filter app.json
+$appfolder = $env:project
+if ($env:appfolder -ne "")
+{
+    $appfolder = Join-Path -Path $env:project -ChildPath $env:appfolder
+    if ((Test-Path -Path $appfolder) -eq $false)
+    {
+        Write-Host "Error cannot find folder containing app to compile."
+        exit(1)
+    }
+}
+Write-Host ("Using '{0}' as project folder" -f $appfolder) 
+
+$AppJsonFile = Get-ChildItem -Path $appfolder -Filter app.json
 $AppJson = Get-Content -Raw -Path $AppJsonFile.FullName | ConvertFrom-Json
 $PublisherName = $AppJson.publisher
 $AppName = $AppJson.name 
 # $ArtifactName = '"' + (Join-Path -Path "C:\tmp\artifact" -ChildPath ("{0}_{1}.app" -f $PublisherName, $AppName)) + '"'
 $ArtifactName = '"' + (Join-Path -Path $env:out -ChildPath ("{0}_{1}.app" -f $PublisherName, $AppName)) + '"'
-
-# There seem to be circumstances where it faster to copy than write directly to share volume
-# $TempErrorLogFile = Join-Path -Path "C:\tmp\logs\" -ChildPath "error.log"
+$invalidChars = [IO.Path]::GetInvalidFileNameChars() -join ''
+$re = "[{0}]" -f [RegEx]::Escape($invalidChars)
+$ArtifactName = $ArtifactName -replace $re
 
 Write-Host "Running alc"
 try
@@ -42,9 +59,9 @@ try
     {
         Write-Host "Found dotnet core compiler." 
         Write-Host "Using dotnet core compiler."
-        \alc\compiler\win32\alc.exe /project:$env:project /out:$ArtifactName /packagecachepath:$env:packagecachepath /assemblyprobingpaths:$alcassemblyProbingPath /errorlog:$env:errorlog
+        \alc\compiler\win32\alc.exe /project:$appfolder /out:$ArtifactName /packagecachepath:$env:packagecachepath /assemblyprobingpaths:$alcassemblyProbingPath /errorlog:$env:errorlog
     } else {
-        \alc\compiler\alc.exe /project:$env:project /out:$ArtifactName /packagecachepath:$env:packagecachepath /assemblyprobingpaths:$alcassemblyProbingPath /errorlog:$env:errorlog
+        \alc\compiler\alc.exe /project:$appfolder /out:$ArtifactName /packagecachepath:$env:packagecachepath /assemblyprobingpaths:$alcassemblyProbingPath /errorlog:$env:errorlog
     }
 } catch {
     Write-Host "An error occurred. Trying to write error log."
